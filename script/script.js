ec9ac84cfb71c3e665a5d23f711a229070fba35e
ilifter <31762817+ilifter@users.noreply.github.com>
2017-10-04 11:55:35 +0200

    Fixat grunddesign för nedre baren

--- script/script.js
+++ script/script.js
@@ -1,3 +1,28 @@
 function registeraccount_onclick(){
 	window.location.href = "register.html";
 }
+
+function traininglog_onclick(){
+	window.location.href = "traininglog.html";
+}
+
+function calculator_onclick(){
+	window.location.href = "calculator.html";
+}
+
+function map_onclick(){
+	window.location.href = "map.html";
+}
+
+function calculator_onclick(){
+	window.location.href = "calculator.html";
+}
+
+function excercises_onclick(){
+	window.location.href = "excercises.html";
+}
+
+function about_onclick(){
+	window.location.href = "about.html";
+}
+
